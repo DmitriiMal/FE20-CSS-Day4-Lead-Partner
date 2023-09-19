e5828b5213810f60e3c1044cf37f56144e9e1326
Viktoria Fedorova <viktoriia.fedorova1@gmail.com>
2023-09-19 13:11:17 +0200

    tried to do the btn with js but gonna redo it

---
+++ Viktoria/basic2.js
@@ -0,0 +1,6 @@
+function openNavbar() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNavbar() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
